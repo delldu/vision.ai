52745b2cfd75de02e75f7193e0d2af2eef8482c2
Dell Du <18588220928@163.com>
2020-08-23 22:53:47 +0800

    Update.

--- mini/test/main.c
+++ mini/test/main.c
@@ -21,21 +21,26 @@ void help(char *cmd)
 	printf("    -h, --help                   Display this help.\n");
 	printf("    -i, --input                  Input image file name.\n");
 	printf("    -o, --output                 Output image file name.\n");
+	printf("    -r, --repeat                 Repeat image file 1000 times.\n");
+
 	exit(1);
 }
 
 int main(int argc, char **argv)
 {
-	int optc;
+	int optc, i;
 	int option_index = 0;
 	char *input_filename = NULL;
 	char *output_filename = NULL;
+	char *repeat_filename = NULL;
+
 	IMAGE *image;
 
 	struct option long_opts[] = {
 		{ "help", 0, 0, 'h'},
 		{ "input", required_argument, 0, 'i'},
 		{ "output", required_argument, 0, 'o'},
+		{ "repeat", required_argument, 0, 'r'},
 		{ 0, 0, 0, 0}
 
 	};
@@ -43,13 +48,16 @@ int main(int argc, char **argv)
 	if (argc <= 1)
 		help(argv[0]);
 	
-	while ((optc = getopt_long(argc, argv, "h i: o:", long_opts, &option_index)) != EOF) {
+	while ((optc = getopt_long(argc, argv, "h i: o: r:", long_opts, &option_index)) != EOF) {
 		switch (optc) {
 		case 'i':
 			input_filename = optarg;
 			break;
 		case 'o':
 			output_filename = optarg;
+			break;
+		case 'r':
+			repeat_filename = optarg;
 			break;
 		case 'h':	// help
 		default:
@@ -64,8 +72,12 @@ int main(int argc, char **argv)
 			image_save(image, output_filename);
 			image_destroy(image);
 		}
-	}
-	else {
+	} else if (repeat_filename) {
+		for (i = 0; i < 1000; i++) {
+			image = image_load(repeat_filename);
+			image_destroy(image);
+		}
+	} else {
 		help(argv[0]);
 	}
 
--- mini/test/test.sh
+++ mini/test/test.sh
@@ -1,40 +1,62 @@
-#/************************************************************************************
+#/************************************************************************************#***
+#***	Copyright Dell 2020, All Rights Reserved.
 #***
-#***	Copyright 2020 Dell(18588220928g@163.com), All Rights Reserved.
-#***
-#***	File Author: Dell, 2020-08-23 17:50:52
+#***	File Author: Dell, 2020年 08月 23日 星期日 22:39:11 CST
 #***
 #************************************************************************************/
 #
 #! /bin/sh
 
-# load bmp
-./test -i images/src.bmp -o images/dst_bmp_bmp.bmp
-./test -i images/src.bmp -o images/dst_bmp_jpg.bmp
-./test -i images/src.bmp -o images/dst_bmp_jpeg.bmp
+func_test()
+{
+	# load bmp
+	./test -i images/src.bmp -o images/dst_bmp_bmp.bmp
+	./test -i images/src.bmp -o images/dst_bmp_jpg.bmp
+	./test -i images/src.bmp -o images/dst_bmp_jpeg.bmp
 
-# load jpg
-./test -i images/src.jpg -o images/dst_jpg_bmp.bmp
-./test -i images/src.jpg -o images/dst_jpg_jpg.bmp
-./test -i images/src.jpg -o images/dst_jpg_jpeg.bmp
+	# load jpg
+	./test -i images/src.jpg -o images/dst_jpg_bmp.bmp
+	./test -i images/src.jpg -o images/dst_jpg_jpg.bmp
+	./test -i images/src.jpg -o images/dst_jpg_jpeg.bmp
 
-# load jpeg
-./test -i images/src.jpeg -o images/dst_jpeg_bmp.bmp
-./test -i images/src.jpeg -o images/dst_jpeg_jpg.bmp
-./test -i images/src.jpeg -o images/dst_jpeg_jpeg.bmp
+	# load jpeg
+	./test -i images/src.jpeg -o images/dst_jpeg_bmp.bmp
+	./test -i images/src.jpeg -o images/dst_jpeg_jpg.bmp
+	./test -i images/src.jpeg -o images/dst_jpeg_jpeg.bmp
 
-# load pbm
-./test -i images/src.pbm -o images/dst_pbm_bmp.bmp
-./test -i images/src.pbm -o images/dst_pbm_jpg.bmp
-./test -i images/src.pbm -o images/dst_pbm_jpeg.bmp
+	# load pbm
+	./test -i images/src.pbm -o images/dst_pbm_bmp.bmp
+	./test -i images/src.pbm -o images/dst_pbm_jpg.bmp
+	./test -i images/src.pbm -o images/dst_pbm_jpeg.bmp
 
-# load pgm
-./test -i images/src.pgm -o images/dst_pgm_bmp.bmp
-./test -i images/src.pgm -o images/dst_pgm_jpg.bmp
-./test -i images/src.pgm -o images/dst_pgm_jpeg.bmp
+	# load pgm
+	./test -i images/src.pgm -o images/dst_pgm_bmp.bmp
+	./test -i images/src.pgm -o images/dst_pgm_jpg.bmp
+	./test -i images/src.pgm -o images/dst_pgm_jpeg.bmp
 
-# load ppm
-./test -i images/src.ppm -o images/dst_ppm_bmp.bmp
-./test -i images/src.ppm -o images/dst_ppm_jpg.bmp
-./test -i images/src.ppm -o images/dst_ppm_jpeg.bmp
+	# load ppm
+	./test -i images/src.ppm -o images/dst_ppm_bmp.bmp
+	./test -i images/src.ppm -o images/dst_ppm_jpg.bmp
+	./test -i images/src.ppm -o images/dst_ppm_jpeg.bmp
+}
 
+perf_test()
+{
+	time ./test -r /tmp/speed.jpg
+}
+
+
+help()
+{
+	echo "Help Test"
+	echo "./test.sh func_test -- Test functions"
+	echo "./test.sh perf_test -- Test performance"
+}
+
+
+if [ "$*" == "" ] ;
+then
+	help
+else
+	eval "$*"
+fi
